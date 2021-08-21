1975908ca5c68faca1f36fb393e767bdbb8feae4
yash gupta <yash3144@gmail.com>
2021-08-22 00:29:49 +0530

    create new enquiry action

---
+++ client/src/actions/enquiryActions.js
@@ -0,0 +1,61 @@
+import axios from 'axios';
+
+export const newEnquiry = (enquiry) => async(dispatch) => {
+
+
+    dispatch({type : 'NEW_ENQUIRY_REQUEST'});
+
+    try {
+        const response = await axios.post('/api/enquiries/newenquiry',enquiry);
+        dispatch({type : 'NEW_ENQUIRY_SUCCESS'});
+
+        console.log(response);
+
+    } catch (error) {
+        dispatch({type : 'NEW_ENQUIRY_FAILED'});
+        console.log(error);
+    }
+}
+
+// export const getUserOrders = () =>async (dispatch,getState) => {
+//     const currentUser = getState().loginUserReducer.currentUser
+//     dispatch({type : 'GET_USER_ORDERS_REQUEST'});
+
+    
+//     try {
+//         const response =await axios.post('/api/orders/getuserorders', {userid : currentUser._id})
+//         console.log(response);
+//         dispatch({type : 'GET_USER_ORDERS_SUCCESS',payload: response.data}) ;
+//     } catch (error) {
+//         dispatch({type : 'GET_USER_ORDERS_FAILED' ,payload: error});
+//     }
+
+
+// }
+
+// export const getAllOrders = () =>async (dispatch,getState) => {
+//     dispatch({type : 'GET_ALL_ORDERS_REQUEST'});
+
+    
+//     try {
+//         const response =await axios.get('/api/orders/getallorders')
+//         console.log(response);
+//         dispatch({type : 'GET_ALL_ORDERS_SUCCESS',payload: response.data}) ;
+//     } catch (error) {
+//         dispatch({type : 'GET_ALL_ORDERS_FAILED' ,payload: error});
+//     }
+
+// }
+
+// export const deliverOrder = (orderid) => async dispatch => {
+
+//     try {
+//         const response = await axios.post('/api/orders/deliverorder', {orderid})
+//         console.log(response);
+//         alert(`Order Delivered`)
+//         const orders =await axios.get('/api/orders/getallorders')
+//         dispatch({type : 'GET_ALL_ORDERS_SUCCESS',payload: orders.data}) ;
+//     } catch (error) {
+//         console.log(error)
+//     }
+// }
