5460a91569015ee9bf3378df59ac6a7a89dc65ca
yash gupta <yash3144@gmail.com>
2021-08-21 21:49:08 +0530

    required imports

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+// import Navbar from './components/Navbar';
+import Homescreen from './screens/Homescreen';
+import {BrowserRouter , Route ,Link ,Switch} from 'react-router-dom';
+// import Cartscreen from './screens/Cartscreen';
+import Registerscreen from './screens/Registerscreen';
+import Loginscreen from './screens/Loginscreen';
+// import Ordersscreen from './screens/Ordersscreen';
+// import Adminscreen from './screens/Adminscreen';
 
 function App() {
   return (
     <div className="App">
-      <h1>Alert</h1>
+      {/* <Navbar /> */}
+      <BrowserRouter>
+        <Route path="/" exact component={Homescreen} />
+        <Route path="/login" exact component={Loginscreen} />
+        <Route path="/registration" exact component={Registerscreen} />
+        
+      </BrowserRouter>
     </div>
   );
 }
